171421f24c66542b10cb043c169623e7efaa2ed6
Babin Bohora <bohorababin1@gmail.com>
2022-04-23 23:10:54 +0900

    controller and route for blog frontend done

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,49 @@
 class BlogsController < ApplicationController
-  
+  before_action :find_blog, only: [ :edit, :update, :destroy ]
+
   def index
     @blogs = policy_scope(Blog)
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_param)
+    authorize @blog
+    if @blog.save
+      redirect_to blogs_path
+   else
+      @blogs = Blog.all
+      render :index     
+   end
+  end
+
+  def edit
+    @blogs = Blog.all
+    authorize @blog
+    render :index
+  end
+
+  def update
+    if @blog.update
+      redirect_to blogs_path 
+    else
+      @blogs = Blog.all
+      render :index 
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
+  private
+
+  def blog_param
+    params.require(:blog).permit(:title, :description, :link, :photo)
+  end
+
+  def find_blog
+    @blog = Blog.find(param[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :blogs, only: [ :index ]
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :blogs, only: [ :index ]
