167b5f239d92d8ad03a2be6fc7c6448f04046c24
Babin Bohora <bohorababin1@gmail.com>
2022-04-26 18:38:23 +0900

    request domain checker added for user api

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -3,8 +3,13 @@ class Api::V1::BlogsController < Api::V1::BaseController
   # before_action :find_blog, only: [:show, :update]
   def index
     @blogs = policy_scope(Blog)
+
+    # Check if the domain is the real domain of user
+    check_request_url(request)
+    # Will render json.jbuilder by default if no errors with url
   end
-  
+
+  # (https?:\/\/)?#{user.last.domain}:3000\/?
   # def show; end
 
   # def update
@@ -15,7 +20,20 @@ class Api::V1::BlogsController < Api::V1::BaseController
   #  end
   # end
   
-  # private 
+  private 
+
+  def check_request_url(request)
+    if request.referrer.nil?
+      render json: {error: "Please, provide referrerPolicy. Check #{profile_url} for your api information."}, status: :unauthorized
+    else
+      request_url = request.referrer.sub(/https?:\/\//, "")
+      unless /(https?:\/\/)?#{current_user.domain}(^(.\w*)|\/\w*)?/.match?(request_url)
+        render :json => {
+          error: "#{request.referrer} is not the correct domain of the user."
+        }, status: :unauthorized
+      end
+    end
+  end
   
   # def find_blog
   #   @blog = Blog.find(params[:id])
