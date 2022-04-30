23c4f62f10bcc83e5ab1e1482841ed7d91eef161
Babin Bohora <bohorababin1@gmail.com>
2022-04-30 16:52:58 +0900

    dev_toggle_controller doing fetch done

--- app/javascript/controllers/dev_toggle_controller.js
+++ app/javascript/controllers/dev_toggle_controller.js
@@ -1,15 +1,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  // static targets = [ "input" ]
 
   connect() {
     console.log("connected")
   }
 
-  toggle() {
-    // console.log("toggle")
-    fetch("/user/toggle")
+  toggle(e) {
+    const crsfToken = document.querySelector('[name="csrf-token"]');
+    fetch("/toggle", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json', 'X-CSRF-Token': crsfToken.content},
+      body: JSON.stringify({ test: e.currentTarget.checked })
+    })
+    .then(response => response.json())
+    .then(data =>{
+      e.target.checked = data.test;
+    }).catch((error) => {
+      console.error('Error:', error);
+    })
   }
 
 
