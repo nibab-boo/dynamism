2e4630cdeb9f7d17bae81bda4ca0e36962cb44cc
Babin Bohora <bohorababin1@gmail.com>
2022-05-12 20:15:38 +0900

    validation test for blog:title :description done

---
+++ spec/models/blogs_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe Blog, type: :model do
+  let(:blog) {
+    blog = Blog.new(title: "Title", description: "New description")
+    blog.valid?
+    user = User.create(email: "test1@gmail.com", password: "123456", confirmed_at: 2.days.ago, created_at: 3.days.ago)
+    blog.update(user: user)
+    return blog
+  }
+
+  describe '#initialize' do
+    
+    context "when valid" do
+      it 'returns a valid Task' do
+        expect(blog.valid?).to eq(true)
+      end
+    end
+
+    context "Invalid Task" do
+      context "without title" do
+        before { blog.title = nil }
+        it "returns invalid task" do
+          expect(blog.valid?).to eq(false)
+        end
+
+        it "returns error message" do
+          blog.valid?
+          expect(blog.errors.messages).to eq({title: ["can't be blank"]})
+        end
+      end
+
+      context "without description" do
+        before { blog.description = nil }
+        it "returns invalid task" do
+          expect(blog.valid?).to eq(false)
+        end
+
+        it "returns error message" do
+          blog.valid?
+          expect(blog.errors.messages).to eq({description: ["can't be blank"]})
+        end
+      end
+    end
+
+  end
+end
