4b80462bff542b5beac7d447408e30af03d7e7a4
Babin Bohora <bohorababin1@gmail.com>
2022-05-14 18:39:09 +0900

    updated user factory with photo and link traits

--- spec/factories/blog.rb
+++ spec/factories/blog.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
-  factory :blog do
+  factory :blog do  
     title {"Title"}
     description {"New description"}
     
     user
+
+    trait :with_photo do
+      photo { Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/test_pic.jpg", 'image/png') }
+    end
+
+    trait :with_link do
+      link { "https://github.com/nibab-boo" }
+    end
     # trait :with_user  do
     #   user
     # end
