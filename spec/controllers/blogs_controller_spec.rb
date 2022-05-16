67f0d4ba31c970fb846da4c6ff4ad67e96d71ce1
Babin Bohora <bohorababin1@gmail.com>
2022-05-16 21:35:14 +0900

    wrote tests for blogs/edit and blogs/create

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe BlogsController, type: :controller do
     end
     
     describe "#create" do
-      context "valid params" do
+      context "Valid params" do
         before { post :create, params: { blog: attributes_for(:blog) }}
 
         it "creates blogs" do
@@ -47,9 +47,9 @@ RSpec.describe BlogsController, type: :controller do
         end
       end
 
-      context "valid params" do
+      context "Invalid params" do
         before { post :create, params: { blog: attributes_for(:blog, title: "")} }
-        
+ 
         it "returns invalid blog" do
           expect(assigns(:blog).id).to eq(nil)
         end
@@ -69,5 +69,85 @@ RSpec.describe BlogsController, type: :controller do
       end
     end
 
+    describe "#edit" do
+      context "Valid Id" do
+        before do
+          blog.save
+          get :edit, params: { id: blog }
+        end
+
+        it "returns successful" do
+          expect(response).to be_successful
+        end
+        
+        it "redirects to index" do
+          expect(response).to render_template('blogs/index')
+        end
+        
+        it "sends a blog with given id" do
+          expect(assigns(:blog).id).to be(blog.id)
+        end
+        
+        it "sends a activerecord relation array of current user" do
+          Array.new(3) { create(:blog, user: user) } 
+          expect(assigns(:blogs)).to match_array(user.blogs.order(id: :asc))
+        end
+      end
+      
+      context "Invalid Id" do
+        let(:action) { get :edit, params: { id: "random" } }
+
+        it "raises exception: RecordNotFound" do
+          expect{action}.to raise_exception(ActiveRecord::RecordNotFound)
+        end
+      end
+    end
+
+    describe "update" do
+      context "Valid params" do
+        before do
+          blog.save
+          put :update, params: { blog: attributes_for(:blog, title: "New Title"), id: blog }
+        end 
+
+        it "updates blog" do
+          blog.reload
+          expect(blog.title).to eq("New Title")
+        end
+
+        it "redirects to index" do
+          expect(response).to redirect_to(blogs_path)
+        end
+
+        it "current user is the owner" do
+          expect(assigns(:blog).user.id).to eq(user.id)
+        end
+      end
+
+      context "Invalid params" do
+        before do
+          blog.save
+          put :update, params: { blog: attributes_for(:blog, title: nil), id: blog}
+        end
+ 
+        it "doesnot update blog" do
+          blog.reload
+          expect(blog.title).to eq("Title")
+        end
+
+        it "returns error message" do
+          expect(assigns(:blog).errors.messages).to eq({title: ["can't be blank"]})
+        end
+
+        it "renders index" do
+          expect(response).to render_template('blogs/index');
+        end
+
+        it "sends a activerecord relation array of current user" do
+          Array.new(3) { create(:blog, user: user) } 
+          expect(assigns(:blogs)).to match_array(user.blogs.order(id: :asc))
+        end
+      end
+    end
   end
 end
