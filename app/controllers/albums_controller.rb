df99a2753787dfd1a8e5599fd180f89e781b910e
Babin Bohora <bohorababin1@gmail.com>
2022-05-26 15:41:12 +0900

    basic structure done

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,32 @@
 class AlbumsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit( :title, :photos )
+  end
 end
