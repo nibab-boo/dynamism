e7aabb2dc442b19f723409bd7edd2b3d2008374e
Babin Bohora <bohorababin1@gmail.com>
2022-05-26 15:50:32 +0900

    user relation added and album migrated

--- db/migrate/20220526061806_create_albums.rb
+++ db/migrate/20220526061806_create_albums.rb
@@ -2,6 +2,7 @@ class CreateAlbums < ActiveRecord::Migration[6.1]
   def change
     create_table :albums do |t|
       t.string :title
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
