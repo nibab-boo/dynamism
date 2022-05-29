f1231a037d2ad3185d74a04e33c6114850388a74
Babin Bohora <bohorababin1@gmail.com>
2022-05-29 12:09:00 +0900

    basic delete for album done

---
+++ app/policies/album_photo_policy.rb
@@ -0,0 +1,12 @@
+class AlbumPhotoPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def destroy?
+    record.album.user === user
+  end
+end
